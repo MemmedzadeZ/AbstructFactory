2334de5a90f3aa217e2dff4d1952ea1c388f910a
Zehra.M <139117868+MemmedzadeZ@users.noreply.github.com>
2023-11-29 19:52:08 +0400

    Update AbstructChair.cs

--- AbstructFactory/AbstructChair.cs
+++ AbstructFactory/AbstructChair.cs
@@ -1,108 +1,186 @@
-﻿namespace AbstructFactory;
+using System;
 
-abstract class AbstructChair{}
-
-class ArtDecoChair:AbstructChair { }
-class VictorianChair : AbstructChair { }
-class ModernChair : AbstructChair { }
-
-
-abstract class AbstructSofa { }
-class ArtDecoSofa:AbstructSofa { }
-class VictorianSofa : AbstructSofa { }
-class ModernSofa : AbstructSofa { }
-
-abstract class AbstructCoffeeTable { }
-class ArtDecoCoffeeTable:AbstructCoffeeTable { }
-class VictorianCoffeeTable:AbstructCoffeeTable { }
-class ModernCoffeeTable : AbstructCoffeeTable { }
-
-
-/////////////////////////////////////////////////////
-
-
-abstract class AbstructFactorys
+// Abstract Product A
+abstract class AbstractChair
 {
-    public abstract AbstructChair CreateChair();
-    public abstract AbstructSofa CreateSofa();
-    public abstract AbstructCoffeeTable CreateCoffeeTable();
-  
-
+    public abstract void Create();
 }
 
-
-class ArtDecoFactorys:AbstructFactorys
+// Concrete Product A1
+class ArtDecoChair : AbstractChair
 {
-    public override AbstructChair CreateChair()
+    public override void Create()
+    {
+        Console.WriteLine("Art Deco Chair created.");
+    }
+}
+
+// Concrete Product A2
+class VictorianChair : AbstractChair
+{
+    public override void Create()
+    {
+        Console.WriteLine("Victorian Chair created.");
+    }
+}
+
+// Concrete Product A3
+class ModernChair : AbstractChair
+{
+    public override void Create()
+    {
+        Console.WriteLine("Modern Chair created.");
+    }
+}
+
+// Abstract Product B
+abstract class AbstractSofa
+{
+    public abstract void Create();
+}
+
+// Concrete Product B1
+class ArtDecoSofa : AbstractSofa
+{
+    public override void Create()
+    {
+        Console.WriteLine("Art Deco Sofa created.");
+    }
+}
+
+// Concrete Product B2
+class VictorianSofa : AbstractSofa
+{
+    public override void Create()
+    {
+        Console.WriteLine("Victorian Sofa created.");
+    }
+}
+
+// Concrete Product B3
+class ModernSofa : AbstractSofa
+{
+    public override void Create()
+    {
+        Console.WriteLine("Modern Sofa created.");
+    }
+}
+
+// Abstract Product C
+abstract class AbstractCoffeeTable
+{
+    public abstract void Create();
+}
+
+// Concrete Product C1
+class ArtDecoCoffeeTable : AbstractCoffeeTable
+{
+    public override void Create()
+    {
+        Console.WriteLine("Art Deco Coffee Table created.");
+    }
+}
+
+// Concrete Product C2
+class VictorianCoffeeTable : AbstractCoffeeTable
+{
+    public override void Create()
+    {
+        Console.WriteLine("Victorian Coffee Table created.");
+    }
+}
+
+// Concrete Product C3
+class ModernCoffeeTable : AbstractCoffeeTable
+{
+    public override void Create()
+    {
+        Console.WriteLine("Modern Coffee Table created.");
+    }
+}
+
+// Abstract Factory
+abstract class AbstractFurnitureFactory
+{
+    public abstract AbstractChair CreateChair();
+    public abstract AbstractSofa CreateSofa();
+    public abstract AbstractCoffeeTable CreateCoffeeTable();
+}
+
+// Concrete Factory 1
+class ArtDecoFactory : AbstractFurnitureFactory
+{
+    public override AbstractChair CreateChair()
     {
         return new ArtDecoChair();
     }
 
-    public override AbstructSofa CreateSofa()
+    public override AbstractSofa CreateSofa()
     {
-      return new ArtDecoSofa();
+        return new ArtDecoSofa();
     }
 
-
-    public override AbstructCoffeeTable CreateCoffeeTable()
+    public override AbstractCoffeeTable CreateCoffeeTable()
     {
         return new ArtDecoCoffeeTable();
     }
 }
 
-
-
-class VictorianFactory : AbstructFactorys
+// Concrete Factory 2
+class VictorianFactory : AbstractFurnitureFactory
 {
-    public override AbstructChair CreateChair()
+    public override AbstractChair CreateChair()
     {
         return new VictorianChair();
     }
 
-    public override AbstructCoffeeTable CreateCoffeeTable()
+    public override AbstractSofa CreateSofa()
+    {
+        return new VictorianSofa();
+    }
+
+    public override AbstractCoffeeTable CreateCoffeeTable()
     {
         return new VictorianCoffeeTable();
     }
-
-    public override AbstructSofa CreateSofa()
-    {
-       return new VictorianSofa();
-    }
 }
 
-
-class ModernFactory : AbstructFactorys
+// Concrete Factory 3
+class ModernFactory : AbstractFurnitureFactory
 {
-    public override AbstructChair CreateChair()
+    public override AbstractChair CreateChair()
     {
         return new ModernChair();
     }
 
-    public override AbstructCoffeeTable CreateCoffeeTable()
+    public override AbstractSofa CreateSofa()
     {
-       return new ModernCoffeeTable();
+        return new ModernSofa();
     }
 
-    public override AbstructSofa CreateSofa()
+    public override AbstractCoffeeTable CreateCoffeeTable()
     {
-       return new ModernSofa();
+        return new ModernCoffeeTable();
     }
 }
 
-
-
 class Client
-{ 
-    private readonly AbstructChair _Chair = null;
-    private readonly AbstructCoffeeTable _CoffeeTable = null;
-    private readonly AbstructSofa _Sofa = null;
+{
+    private readonly AbstractChair _chair;
+    private readonly AbstractCoffeeTable _coffeeTable;
+    private readonly AbstractSofa _sofa;
 
-    public Client(AbstructFactorys abstructFactorys)
+    public Client(AbstractFurnitureFactory furnitureFactory)
     {
-        _Chair = abstructFactorys.CreateChair();
-        _Sofa = abstructFactorys.CreateSofa();
-        _CoffeeTable = abstructFactorys.CreateCoffeeTable();
+        _chair = furnitureFactory.CreateChair();
+        _sofa = furnitureFactory.CreateSofa();
+        _coffeeTable = furnitureFactory.CreateCoffeeTable();
     }
 
-   
+    public void Run()
+    {
+        _chair.Create();
+        _sofa.Create();
+        _coffeeTable.Create();
+    }
 }
